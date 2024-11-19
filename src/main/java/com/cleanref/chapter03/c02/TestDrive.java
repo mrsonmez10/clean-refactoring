3bcd20d0a432dceae9313f01c1eccb9d6088b3a2
mrsonmez10 <frknsnmzz@hotmail.com.tr>
2024-11-19 20:13:06 +0300

    - comment removed

--- src/main/java/com/cleanref/chapter03/c02/TestDrive.java
+++ src/main/java/com/cleanref/chapter03/c02/TestDrive.java
@@ -4,8 +4,6 @@ public class TestDrive {
 
     public static void main(String[] args) {
 
-        // Kodlarımız: Yukarıdan Aşağı Şiir veya gazete gibi Akmalı.
-
         // Autowired vs constructor
 
     }
